1eec70f5c0cca8693a93e9b88361998291cd35bc
dmartielli <dmartielli@thegraysonschool.org>
2023-09-28 15:00:12 -0400

    started

--- uhoh.py
+++ uhoh.py
@@ -1,7 +1,9 @@
 import csv
 from time import perf_counter
-
-
+import random
+from random import shuffle
+import matplotlib
+from matplotlib import pyplot as plt
 def fact(n):
     if n>1:
         return fact(n-1) + fact(n-2)
@@ -17,31 +19,53 @@ def fib_bad(n):
 def fib_meh(n):
   a = 0
   b = 1
-  print(a)
+  d=[0]
   if n >1:
-    print(b)
+    d.append(b)
     for i in range(n):
       c=a+b
-      print(c)
+      d.append(c)
       a=b
       b=c
+  return d
 #no look#
 
+
+aa = []
+for i in range(10):
+    aa.append(random.randint(1,9))
+def dostuff():
+    runtime = perf_counter()
+    aa.sort()
+    runtime = perf_counter() - runtime
+    print(runtime)
+    shuffle(aa)
+
 def mian():
-    with open("/Users/dmartielli/Documents/PyCharm/for_coding_1.csv" , 'w') as csvfile:
+    with open("/Users/dmartielli/Documents/Stuff/PyCharm/for_coding_1.csv" , 'w') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(["input", "meh time", "bad time"])
+        ok = []
+        nokay = []
         for i in range(34):
             exe_time = perf_counter()
             fib_val = fib_bad(i)
             exe_time_bad = perf_counter() - exe_time
+            nokay.append(exe_time_bad)
             exe_time = perf_counter()
             fib_val = fib_meh(i)
             exe_time = perf_counter() - exe_time
+            ok.append(exe_time)
             csvwriter.writerow([i, exe_time,exe_time_bad])
+        plt.plot(nokay)
+        plt.plot(ok)
+        plt.show()
+    dostuff()
+
 
 
 
 if __name__== "__main__":
     mian()
+
 
